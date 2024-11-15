6f9868323affec59a782de96c941d0456709f551
eliyahuri <tkhvu1236@gmail.com>
2024-11-15 12:59:54 +0200

    initialize type-safe-groupby project with TypeScript setup, npm configuration, and essential files

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+const extractKey = <K extends PropertyKey, T>(
+  item: T,
+  index: number,
+  keySelector: keyof T | ((item: T, index: number) => K)
+): K => {
+  return typeof keySelector === "function"
+    ? keySelector(item, index)
+    : (item[keySelector] as K);
+};
+
+export const groupByFunction = <K extends PropertyKey, T>(
+  items: Iterable<T>,
+  keySelector: keyof T | ((item: T, index: number) => K)
+): Partial<Record<K, T[]>> => {
+  const result: Partial<Record<K, T[]>> = {};
+
+  let index = 0;
+  for (const item of items) {
+    const key = extractKey(item, index, keySelector);
+    result[key] ||= [];
+    result[key]!.push(item);
+    index++;
+  }
+
+  return result;
+};
